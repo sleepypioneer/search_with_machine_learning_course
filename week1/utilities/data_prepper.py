0a0cef0ed754290466aefcee0b9b5c9e27f9ce0e
Jessica Greene <jessica0greene@gmail.com>
2022-06-25 18:34:27 +0000

    step4

--- week1/utilities/data_prepper.py
+++ week1/utilities/data_prepper.py
@@ -1,11 +1,12 @@
 # This file processes our queries, runs them through OpenSearch against the BBuy Products index to fetch their "rank" and so they can be used properly in a click model
+import json
+import os
 
 import ltr_utils as lu
 import numpy as np
 import pandas as pd
 import query_utils as qu
 from opensearchpy import RequestError
-import os
 
 # from importlib import reload
 
@@ -231,24 +232,35 @@ class DataPrepper:
                                                 self.ltr_store_name,
                                                 size=len(query_doc_ids), terms_field=terms_field)
         ##### Step Extract LTR Logged Features:
-        # IMPLEMENT_START --
-        print("IMPLEMENT ME: __log_ltr_query_features: Extract log features out of the LTR:EXT response and place in a data frame")
-        # Loop over the hits structure returned by running `log_query` and then extract out the features from the response per query_id and doc id.  Also capture and return all query/doc pairs that didn't return features
-        # Your structure should look like the data frame below
+        response = self.opensearch.search(body=log_query, index=self.index_name)
+
+        hits = response["hits"]["hits"]
         feature_results = {}
         feature_results["doc_id"] = []  # capture the doc id so we can join later
-        feature_results["query_id"] = []  # ^^^
+        feature_results["query_id"] = []
         feature_results["sku"] = []
-        feature_results["name_match"] = []
-        rng = np.random.default_rng(12345)
+        
+        ##### Step 4.f:
+        # Loop over the hits structure returned by running `log_query` and then extract out the features from the response per query_id and doc id.  
+        # Also capture and return all query/doc pairs that didn't return features
+        # Your structure should look like the data frame below
         for doc_id in query_doc_ids:
             feature_results["doc_id"].append(doc_id)  # capture the doc id so we can join later
             feature_results["query_id"].append(query_id)
-            feature_results["sku"].append(doc_id)  
-            feature_results["name_match"].append(rng.random())
+            feature_results["sku"].append(doc_id)
+            for hit in hits:
+                if hit["_id"] == doc_id:
+                    log_entries = hit["fields"]["_ltrlog"][0]["log_entry"]
+                    features = [log_entry["name"] for log_entry in log_entries]
+                    for feature in features:
+                        feature_value = next((item["value"] for item in log_entries if item['name'] == "name_match"), 0)
+                        if feature in feature_results.keys():
+                            feature_results["name_match"].append(feature_value)
+                        else:
+                            feature_results[feature] = [feature_value]
+
         frame = pd.DataFrame(feature_results)
         return frame.astype({'doc_id': 'int64', 'query_id': 'int64', 'sku': 'int64'})
-        # IMPLEMENT_END
 
     # Can try out normalizing data, but for XGb, you really don't have to since it is just finding splits
     def normalize_data(self, ranks_features_df, feature_set, normalize_type_map):
